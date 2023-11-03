bb9279eeb42e33dd2bf70a0659fed9006689ec40
Andrew <804023@sylvaniastudents.org>
2023-11-03 10:22:39 -0400

    update

--- ScoreAmount.cs
+++ ScoreAmount.cs
@@ -5,19 +5,26 @@ using UnityEngine.UI;
 
 public class ScoreAmount : MonoBehaviour
 {
+    [SerializeField]
+    public Text MyScore;
+    [SerializeField]
     public int numerator = 0;
     public static int denominator = 0;
     //public static int score = numerator / denominator;
-    Text MyScore;
+    
     // Start is called before the first frame update
     void Start()
     {
         MyScore.text = "";
     }
 
+    public void AddScore()
+    {
+        MyScore.text = "Score: " + numerator;
+    }
     // Update is called once per frame
     void Update()
     {
-        MyScore.text = "Score: " + numerator;
+ 
     }
 }
